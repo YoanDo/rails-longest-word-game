5353bf5ccf82644b88a4657e27953e6841ba9bb2
Yoan Dorny <y.n.dorny@gmail.com>
2017-08-07 22:26:59 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'score', to: 'longest_game#score'
+  get 'game', to: 'longest_game#game'
+  root to: 'longest_game#game'
 end
